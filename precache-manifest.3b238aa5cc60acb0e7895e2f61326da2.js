37caff8d55f62caf283ba2e27aff67720ef66695
unknown <tasbihaasim@gmail.com>
2023-05-15 17:53:18 +0300

    Updates

---
+++ precache-manifest.3b238aa5cc60acb0e7895e2f61326da2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42169a01a4efb0cba52f",
+    "url": "/static/css/main.0fa3bd7c.chunk.css"
+  },
+  {
+    "revision": "42169a01a4efb0cba52f",
+    "url": "/static/js/main.42169a01.chunk.js"
+  },
+  {
+    "revision": "a89266deb73d87b01859",
+    "url": "/static/js/1.a89266de.chunk.js"
+  },
+  {
+    "revision": "ae4bef603ed213a1079f",
+    "url": "/static/js/2.ae4bef60.chunk.js"
+  },
+  {
+    "revision": "7f2d4e2d2fa34e27db68",
+    "url": "/static/js/runtime~main.7f2d4e2d.js"
+  },
+  {
+    "revision": "4c860b6a69172e1ba3bfc73ce87dd766",
+    "url": "/index.html"
+  }
+];
