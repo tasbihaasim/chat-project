94851ab8187d19e5dc1691009218afd1b19896f0
unknown <tasbihaasim@gmail.com>
2023-05-15 18:09:01 +0300

    Updates

---
+++ precache-manifest.aea9067af5151355a7d2444af929df7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42169a01a4efb0cba52f",
+    "url": "/chat-project/static/css/main.0fa3bd7c.chunk.css"
+  },
+  {
+    "revision": "42169a01a4efb0cba52f",
+    "url": "/chat-project/static/js/main.42169a01.chunk.js"
+  },
+  {
+    "revision": "a89266deb73d87b01859",
+    "url": "/chat-project/static/js/1.a89266de.chunk.js"
+  },
+  {
+    "revision": "ae4bef603ed213a1079f",
+    "url": "/chat-project/static/js/2.ae4bef60.chunk.js"
+  },
+  {
+    "revision": "8404373b9c56827b31dd",
+    "url": "/chat-project/static/js/runtime~main.8404373b.js"
+  },
+  {
+    "revision": "35b9628f6e035a807adcfbdd495e9601",
+    "url": "/chat-project/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3b238aa5cc60acb0e7895e2f61326da2.js"
+  "/chat-project/precache-manifest.aea9067af5151355a7d2444af929df7b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/chat-project/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
